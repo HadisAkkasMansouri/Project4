27b3bb826cb6736b73661d25b3a58f4f80c66510
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-29 17:34:00 +0430

    hi

--- src/main/java/ir/dotin/business/LoanTypeValidation.java
+++ src/main/java/ir/dotin/business/LoanTypeValidation.java
@@ -1,17 +1,21 @@
 package ir.dotin.business;
 
 import ir.dotin.dataaccess.entity.LoanType;
+import ir.dotin.exception.InvalidEntranceException;
 import ir.dotin.exception.NullRequiredFieldException;
 
 public class LoanTypeValidation {
 
-    public static LoanType validateLoanTypeFields(String loanTypeName, Float interestRate) throws NullRequiredFieldException {
+    public static LoanType validateLoanTypeFields(String loanTypeName, Float interestRate) throws NullRequiredFieldException, InvalidEntranceException {
 
         if (loanTypeName.isEmpty()){
             throw new NullRequiredFieldException("پر کردن فیلد نام نوع تسهیلات الزامی است");
         }
         if (interestRate == null){
             throw new NullRequiredFieldException("پر کردن فیلد نرخ سود الزامی است");
+        }
+        if(interestRate >= 100){
+            throw new InvalidEntranceException("نرخ سود باید کمتر از ۱۰۰ درصد باشد");
         }
         return new LoanType(loanTypeName, interestRate);
     }

4191ee4db09fc9d9253f408e835d57d5ad96cce7
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-08-30 11:49:00 +0430

    hi

---
+++ src/main/java/ir/dotin/business/GrantConditionValidation.java
@@ -0,0 +1,39 @@
+package ir.dotin.business;
+
+import ir.dotin.dataaccess.entity.GrantCondition;
+import ir.dotin.exception.NotInRangeException;
+import ir.dotin.exception.NullRequiredFieldException;
+
+import java.util.ArrayList;
+
+public class GrantConditionValidation {
+
+    public static GrantCondition validateGrantCondition(ArrayList<GrantCondition> grantConditions) throws NullRequiredFieldException, NotInRangeException {
+
+        for (GrantCondition grantCondition : grantConditions) {
+
+            if (grantCondition.getGrantConditionName() == null) {
+                throw new NullRequiredFieldException("وارد نمودن فیلد نام شرط اعطا الزامی است");
+            }
+            if (grantCondition.getMinAmount() == null) {
+                throw new NullRequiredFieldException("وارد نمودن فیلد حداقل مبلغ قرارداد الزامی است");
+            }
+            if (grantCondition.getMaxAmount() == null) {
+                throw new NullRequiredFieldException("وارد نمودن فیلد حداکثر مبلغ قرارداد الزامی است");
+            }
+            if (grantCondition.getMinDuration() == 0) {
+                throw new NullRequiredFieldException("وارد نمودن فیلد حداقل مدت قرارداد الزامی است");
+            }
+            if (grantCondition.getMaxDuration() == 0) {
+                throw new NullRequiredFieldException("وارد نمودن فیلد حداکثر مدت قرارداد الزامی است");
+            }
+            if (grantCondition.getMinDuration() > grantCondition.getMaxDuration()){
+                throw new NotInRangeException("حداکثر مدت قرداد باید از حداقل مدت قرداد بزرگتر باشد");
+            }
+            if ((grantCondition.getMinAmount().compareTo(grantCondition.getMaxAmount())) <= 0){
+                throw new NotInRangeException("حداکثر مبلغ قرداد باید از حداقل مبلغ قرداد بزرگتر باشد");
+            }
+        }
+        return new GrantCondition();
+    }
+}

0835eed79194b5614bb8ad69963d193ae2caa9dc
Reza Sadeghi <hadis.mansouri@gmail.com>
2016-09-05 15:12:11 +0430

    hi

---
+++ src/main/java/ir/dotin/utility/LoggerUtil.java
@@ -0,0 +1,15 @@
+package ir.dotin.utility;
+
+
+import org.apache.log4j.Logger;
+
+public class LoggerUtil {
+
+    private static Logger logger = Logger.getLogger("Log");
+
+    private LoggerUtil() {}
+
+    public static Logger getLogger() {
+        return logger;
+    }
+}
